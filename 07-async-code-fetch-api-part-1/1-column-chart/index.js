33cd3b05745ff0bad29686c32bb972c2938fce29
Dmitriy Bekhterev <dbehterev@gmail.com>
2022-05-22 13:30:01 +0300

    07\1 - доделка

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -3,38 +3,122 @@ import fetchJson from "./utils/fetch-json.js";
 const BACKEND_URL = "https://course-js.javascript.ru";
 
 export default class ColumnChart {
-  constructor(
-    {
-      data = [],
-      label = "",
-      value = "",
-      link = "",
-      formatHeading = (val) => val,
-      chartHeight = 50,
-    } = {
-      data: [],
-      label: "",
-      value: "",
-      link: "",
-      formatHeading: (val) => val,
-      chartHeight: 50,
-    }
-  ) {
-    this.data = data;
+  constructor({
+    data = [],
+    label = "",
+    value = "",
+    link = "",
+    formatHeading = (val) => val,
+    chartHeight = 50,
+    url = "",
+    range = {
+      from: new Date(),
+      to: new Date(),
+    },
+  } = {}) {
+    this.url = url;
     this.label = label;
     this.value = value;
     this.link = link;
     this.formatHeading = formatHeading;
     this.chartHeight = chartHeight;
+    this.data = data;
     this.element = this.getElement();
+
+    // в угоду тестирующей функции, не понимаю, зачем это нужно
+    this.subElements = this.getSubelements();
+
     this.divChartColumn = null;
+    this.range = range;
+
+    if (this.url) {
+      this.data = this.getData(range.from, range.to);
+    }
+  }
+
+  getSubelements() {
+    const subElements = {};
+    const allSubElements = this.element.querySelectorAll("[data-element]");
+    allSubElements.forEach((subEl) => {
+      subElements[subEl.dataset.element] = subEl;
+    });
+
+    return subElements;
+  }
+
+  compoundUrl(from, to) {
+    return (
+      BACKEND_URL +
+      "/" +
+      this.url +
+      "?" +
+      "from=" +
+      from.toISOString() +
+      "&to=" +
+      to.toISOString()
+    );
+
+    //console.log(URL);
+  }
+
+  calcHeaderVal(data) {
+    if (Array.isArray(data)) {
+      return data.reduce((acc, el) => acc + el, 0);
+    } else if (ColumnChart.thisIdObject(data)) {
+      return Object.values(data).reduce((acc, val) => acc + val, 0);
+    }
+  }
+
+  async getData(from, to) {
+    this.element.classList.add("column-chart_loading");
+    const header = this.element.querySelector('[data-element="header"]');
+    header.textContent = "";
+
+    const body = this.element.querySelector('[data-element="body"]');
+    // удалим все дочерние элементы из DOM
+    body.innerHTML = "";
+
+    // пропишем новые даты
+    this.range.from = from;
+    this.range.to = to;
+
+    try {
+      // ждем результата промиса по загрузке данных
+      this.data = await fetchJson(this.compoundUrl(from, to));
+      //console.dir(this.data);
+
+      // задать заголовок
+      header.textContent = this.formatHeading(this.calcHeaderVal(this.data));
+
+      // задать график на основе данных
+      this.renderCharts();
+      const divChartContainer = this.element.querySelector(
+        ".column-chart__container"
+      );
+
+      body.innerHTML = this.divChartColumn.innerHTML;
+
+      // убираем класс загрузки
+      this.element.classList.remove("column-chart_loading");
+    } catch (err) {
+      console.error("произошла ошибка при загрузке данных: ", err.message);
+      return;
+    }
+
+    return this.data;
   }
 
   renderCharts() {
-    const maxValue = Math.max(...this.data);
+    let maxValue = 0;
+    if (Array.isArray(this.data)) {
+      maxValue = Math.max(...this.data);
+    } else if (ColumnChart.thisIdObject(this.data)) {
+      maxValue = Math.max(...Object.values(this.data));
+    }
+
     let scale;
     if (maxValue) {
-      scale = 50 / maxValue;
+      scale = this.chartHeight / maxValue;
     }
 
     if (this.divChartColumn) {
@@ -49,23 +133,41 @@ export default class ColumnChart {
       className: "column-chart__chart",
     });
 
-    this.data.forEach((val) => {
-      let divEl = this.createEl({
-        type: "div",
-        style: `--value: ${Math.floor(val * scale)}`,
-        dataTooltip: `${((val / maxValue) * 100).toFixed(0)}%`,
-      });
+    if (Array.isArray(this.data)) {
+      this.data.forEach((val) => {
+        const divEl = this.createEl({
+          type: "div",
+          style: `--value: ${Math.floor(val * scale)}`,
+          dataTooltip: `${((val / maxValue) * 100).toFixed(0)}%`,
+        });
 
-      this.divChartColumn.append(divEl);
-    });
+        this.divChartColumn.append(divEl);
+      });
+    } else if (ColumnChart.thisIdObject(this.data)) {
+      for (let [key, val] of Object.entries(this.data)) {
+        const divEl = this.createEl({
+          type: "div",
+          style: `--value: ${Math.floor(val * scale)}`,
+          dataTooltip: `<div><small>${key.toLocaleString("default", {
+            dateStyle: "medium",
+          })}</small></div><strong>${((val / maxValue) * 100).toFixed(
+            0
+          )}</strong>`,
+        });
+
+        this.divChartColumn.append(divEl);
+      }
+    }
   }
 
-  update(newData) {
-    if (!Array.isArray(newData)) {
-      return;
-    }
-    this.data = newData;
-    this.renderCharts();
+  async update(from, to) {
+    return await this.getData(from, to);
+    // старый код с прошлой задачи, оставлю
+    // if (!Array.isArray(newData)) {
+    //   return;
+    // }
+    // this.data = newData;
+    // this.renderCharts();
   }
 
   destroy() {
@@ -122,13 +224,36 @@ export default class ColumnChart {
     return el;
   }
 
+  static thisIdObject(obj) {
+    if (typeof obj === "object" && !Array.isArray(obj) && obj !== null) {
+      return true;
+    }
+
+    return false;
+  }
+
+  dataIsEmpty() {
+    if (Array.isArray(this.data) && this.data.length === 0) {
+      return true;
+    } else if (
+      ColumnChart.thisIdObject(this.data) &&
+      Object.keys(this.data).length === 0 &&
+      Object.getPrototypeOf(this.data) === Object.prototype
+    ) {
+      return true;
+    }
+    // можно еще на другие типы проверить, но в контексте задачи это не требуется
+
+    return false;
+  }
+
   getElement() {
     let div;
 
     div = this.createEl({
       type: "div",
       className: `column-chart${
-        this.data.length === 0 ? " column-chart_loading" : ""
+        this.dataIsEmpty() ? " column-chart_loading" : ""
       }`,
       style: "--chart-height: 50",
     });
