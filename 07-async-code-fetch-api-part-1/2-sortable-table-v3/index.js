2dbe2e1b5f8fdfedf002b490cbdcd74dc05bc53d
Dmitriy Bekhterev <dbehterev@gmail.com>
2022-05-24 19:36:32 +0300

    07\2 - удалил лишние блоки

--- 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
+++ 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
@@ -2,23 +2,6 @@ import fetchJson from "./utils/fetch-json.js";
 
 const BACKEND_URL = "https://course-js.javascript.ru";
 
-// export default class SortableTable {
-//   constructor(headersConfig, {
-//     data = [],
-//     sorted = {}
-//   } = {}) {
-
-//   }
-
-//   sortOnClient (id, order) {
-
-//   }
-
-//   sortOnServer (id, order) {
-
-//   }
-// }
-
 async function scrollHandler() {
   const bottom = this.element.getBoundingClientRect().bottom;
 
